09bf9c63b2879fdee1709696d2ab029235893285
jorgipons <jorgibeni7@gmail.com>
2026-03-10 18:02:26 +0100

    feat: Implement PWA with Firebase for basketball manager application, including messaging service worker and main application UI.

--- basketball-manager/firebase-messaging-sw.js
+++ basketball-manager/firebase-messaging-sw.js
@@ -1,6 +1,38 @@
 importScripts('https://www.gstatic.com/firebasejs/11.2.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.2.0/firebase-messaging-compat.js');
 
+// Service Worker for offline caching (Consolidated)
+const CACHE_NAME = 'basket-manager-v2';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://unpkg.com/react@18/umd/react.development.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js',
+    'https://cdn.tailwindcss.com',
+    'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
 firebase.initializeApp({
     apiKey: "AIzaSyAAbaUEbPjltfQrDethVojxoxD1gj4AC0w",
     authDomain: "basketmanager-ed370.firebaseapp.com",
@@ -22,3 +54,4 @@ messaging.onBackgroundMessage((payload) => {
 
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
